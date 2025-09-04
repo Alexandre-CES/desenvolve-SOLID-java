e420bbe3d55f393752d3d10a68c46a5fb989bf5d
Alexandre <alexandreces666@gmail.com>
2025-09-04 12:53:30 -0300

    feat: interface Segregation 2

--- segregacaoInterface/src/br/com/isp/Main.java
+++ segregacaoInterface/src/br/com/isp/Main.java
@@ -4,7 +4,18 @@ package br.com.isp;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Corrente corrente = new Corrente();
+        Poupanca poupanca = new Poupanca();
 
+        System.out.println("Realizando operações na conta corrente");
+        corrente.depositar(1000);
+        corrente.sacar(200);
+        corrente.calculaJuros();
+
+        System.out.println("Realizando operações na conta Poupança");
+        poupanca.depositar(1000);
+        poupanca.sacar(200);
+        poupanca.calculaJuros();
 
     }
 }
---
+++ segregacaoInterface/src/br/com/isp/Poupanca.java
@@ -0,0 +1,23 @@
+package br.com.isp;
+
+public class Poupanca implements Conta {
+    @Override
+    public void depositar(double valor) {
+        System.out.println("Depositando um poupanca");
+    }
+
+    @Override
+    public void sacar(double valor) {
+        System.out.println("Sacando um poupanca");
+    }
+
+    @Override
+    public void transferir(double valor, Conta destino) {
+        System.out.println("Transferrando um poupanca");
+    }
+
+    @Override
+    public void calculaJuros() {
+        System.out.println("calculando juros");
+    }
+}
